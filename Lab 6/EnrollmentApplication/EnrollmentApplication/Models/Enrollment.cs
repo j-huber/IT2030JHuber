7880ed943f6f4b447b358bfa96bf01a35301200b
j-huber <jason.huber00@gmail.com>
2019-10-07 00:10:52 -0400

    Finish Lab 6
    Questions Asked In BlackBoard

---
+++ Lab 6/EnrollmentApplication/EnrollmentApplication/Models/Enrollment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EnrollmentApplication.Models
+{
+    public class Enrollment
+    {
+        public virtual int EnrollmentID { get; set; }
+        public virtual int StudentID { get; set; }
+        public virtual int CourseID { get; set; }
+
+        [Required]
+        [RegularExpression(@"[A-F]", ErrorMessage = "Please enter a valid grade of A-F.")]
+        public virtual char Grade { get; set; }
+        public virtual Student Student { get; set; }
+        public virtual Course Course { get; set; }
+        public virtual bool IsActive { get; set; }
+
+        [Required(ErrorMessage ="Your Campus is required.")]
+        [Display(Name = "Assigned Campus")]
+        public virtual string AssignedCampus { get; set; }
+
+        [Required (ErrorMessage ="Your Semester is required.")]
+        [Display(Name = "Enrolled in Semester")]
+        public virtual string EnrollmentSemester { get; set; }
+
+        [Required]
+        [Range(2018, 9999, ErrorMessage ="The year entered cannot be before than 2018.")]
+        public virtual int EnrollmentYear { get; set; }
+    }
+}
---
+++ Lab 6/EnrollmentApplication/EnrollmentApplication/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EnrollmentApplication.Models
+{
+    public class Student
+    {
+        [Display(Name ="Student ID")]
+        public virtual int StudentID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage ="The last name you entered is too long.")]
+        [Display(Name ="Last Name")]
+        public virtual string StudentLastName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage ="The first name you entered is too long.")]
+        [Display(Name = "First Name")]
+        public virtual string StudentFirstName { get; set; }
+    }
+}
