1651873764461e2fcd37fa61d3533754e1868b7c
j-huber <jason.huber00@gmail.com>
2019-10-27 23:01:06 -0400

    Complete Lab 7 Asked Questions

---
+++ Lab 7/EnrollmentApplication/EnrollmentApplication/Models/EnrollmentDBInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnrollmentApplication.Models
+{
+    public class EnrollmentDBInitializer : System.Data.Entity.DropCreateDatabaseAlways<EnrollmentDB>
+    {
+        protected override void Seed(EnrollmentDB context)
+        {
+            context.Courses.Add(new Course { CourseTitle = "English", CourseCredits = 3, CourseDescription = "English w Honors", CourseID = 1 });
+            context.Courses.Add(new Course { CourseTitle = "Math", CourseCredits = 3, CourseDescription = "Math w Honors", CourseID = 2 });
+            context.Courses.Add(new Course { CourseTitle = "Science", CourseCredits = 3, CourseDescription = "Science w Honors", CourseID = 3});
+            context.Courses.Add(new Course { CourseTitle = "History", CourseCredits = 3, CourseDescription = "History w Honors", CourseID = 4 });
+            context.Courses.Add(new Course { CourseTitle = "Philosophy", CourseCredits = 3, CourseDescription = "Philosophy w Honors", CourseID = 5 });
+           context.Students.Add(new Student { StudentID = 1, StudentFirstName = "Jason", StudentLastName = "Huber" });
+            context.Students.Add(new Student { StudentID = 2, StudentFirstName = "Joseph", StudentLastName = "Hocevar" });
+            context.Students.Add(new Student { StudentID = 3, StudentFirstName = "Alice", StudentLastName = "Smith" });
+            context.Students.Add(new Student { StudentID = 4, StudentFirstName = "Alex", StudentLastName = "Gonzalez" });
+           context.Students.Add(new Student { StudentID = 5, StudentFirstName = "Ryan", StudentLastName = "Splitt" });
+
+            base.Seed(context);
+        }
+    }
+}
---
+++ Lab 7/EnrollmentApplication/EnrollmentApplication/Models/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EnrollmentApplication.Models
+{
+    public class Student : IValidatableObject
+    {
+        [Display(Name ="Student ID")]
+        public virtual int StudentID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage ="The last name you entered is too long.")]
+        [Display(Name ="Last Name")]
+        public virtual string StudentLastName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage ="The first name you entered is too long.")]
+        [Display(Name = "First Name")]
+        public virtual string StudentFirstName { get; set; }
+
+        [MinimumAge(20)]
+        public virtual int Age { get; set; }
+
+        public virtual string Address1 { get; set; }
+
+        public virtual string Address2 { get; set; }
+
+        public virtual string City { get; set; }
+
+        public virtual string Zipcode { get; set; }
+
+        public virtual string State { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            //validate address
+            if (Address1.Equals(Address2))
+            {
+                yield return (new ValidationResult("Address 1 cannot be the same as Address 2"));
+            }
+
+            //validate state minimum
+            if (State.Length != 2)
+            {
+                yield return (new ValidationResult("Enter a 2 digit State Code"));
+            }
+
+             //validate zipcode
+            if (Zipcode.Length != 5)
+            {
+                yield return (new ValidationResult("Enter a 5 digit Zipcode"));
+            }
+            
+        }
+    }
+}
